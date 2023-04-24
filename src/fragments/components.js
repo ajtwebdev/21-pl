9f7eb61271a2d22e8021b8e0ec694bec0c717301
Pavel Saparov <saparov.p@gmail.com>
2023-04-24 21:49:05 +0200

    Minor formating of graphql fragments

--- src/fragments/components.js
+++ src/fragments/components.js
@@ -1,36 +1,24 @@
 
-    import { graphql } from 'gatsby'
+import { graphql } from 'gatsby'
   
-    export const componentFragments = graphql`
-       
- 
-      fragment Page_Badges on WpPage_Pagecomponents_PageComponents_Badges {
-        
-      title
-    
+export const componentFragments = graphql`fragment Page_Badges on WpPage_Pagecomponents_PageComponents_Badges {
+  title
+}
+
+fragment Page_Banner on WpPage_Pagecomponents_PageComponents_Banner {
+  title
+  description
+  subheader
+  image {
+    localFile {
+      childImageSharp {
+        gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
       }
-     
- 
-      fragment Page_Banner on WpPage_Pagecomponents_PageComponents_Banner {
-        
-        title
-        description
-        subheader
-        image {
-          localFile {
-            childImageSharp {
-              fluid {
-                src
-              }
-            }
-          }
-        }
-      
-      }
-     
- 
-      fragment Page_Benefits1 on WpPage_Pagecomponents_PageComponents_Benefits1 {
-        
+    }
+  }
+}
+
+fragment Page_Benefits1 on WpPage_Pagecomponents_PageComponents_Benefits1 {
   subheader
   title
   benefitContent {
@@ -45,32 +33,26 @@
       }
     }
   }
-  
-      }
-     
- 
-      fragment Page_Carousel on WpPage_Pagecomponents_PageComponents_Carousel {
-        
-    subheader
+}
+
+fragment Page_Carousel on WpPage_Pagecomponents_PageComponents_Carousel {
+  subheader
+  title
+  carouselContent {
     title
-    carouselContent {
-      title
-      description
-      image {
-        altText
-        localFile {
-          childImageSharp {
-            gatsbyImageData
-          }
+    description
+    image {
+      altText
+      localFile {
+        childImageSharp {
+          gatsbyImageData
         }
       }
     }
-  
-      }
-     
- 
-      fragment Page_ComponentA on WpPage_Pagecomponents_PageComponents_ComponentA {
-        
+  }
+}
+
+fragment Page_ComponentA on WpPage_Pagecomponents_PageComponents_ComponentA {
   body
   subheader
   title
@@ -87,62 +69,50 @@
       }
     }
   }
-    
-      }
-     
- 
-      fragment Page_ComponentB on WpPage_Pagecomponents_PageComponents_ComponentB {
-        
-    body
-    subheader
+}
+
+fragment Page_ComponentB on WpPage_Pagecomponents_PageComponents_ComponentB {
+  body
+  subheader
+  title
+  componentItems {
     title
-    componentItems {
-      title
-      text
-      button
-      image {
-        altText
-        localFile {
-          childImageSharp {
-            gatsbyImageData
-          }
+    text
+    button
+    image {
+      altText
+      localFile {
+        childImageSharp {
+          gatsbyImageData
         }
       }
     }
-      
+  }
+}
+
+fragment Page_ComponentC on WpPage_Pagecomponents_PageComponents_ComponentC {
+  subheader
+  title
+  body
+  image {
+    altText
+    localFile {
+      childImageSharp {
+        gatsbyImageData
       }
-     
- 
-      fragment Page_ComponentC on WpPage_Pagecomponents_PageComponents_ComponentC {
-        
-        subheader
-        title
-        body
-        image {
-            altText
-            localFile {
-                childImageSharp {
-                    gatsbyImageData
-                }
-            }
-        }
-    
-      }
-     
- 
-      fragment Page_ComponentD on WpPage_Pagecomponents_PageComponents_ComponentD {
-        
-    subheader
-    title
-    content {
-        text
     }
-  
-      }
-     
- 
-      fragment Page_ComponentE on WpPage_Pagecomponents_PageComponents_ComponentE {
-        
+  }
+}
+
+fragment Page_ComponentD on WpPage_Pagecomponents_PageComponents_ComponentD {
+  subheader
+  title
+  content {
+    text
+  }
+}
+
+fragment Page_ComponentE on WpPage_Pagecomponents_PageComponents_ComponentE {
   subheader
   title
   content {
@@ -157,12 +127,9 @@
       }
     }
   }
-    
-      }
-     
- 
-      fragment Page_Cta1 on WpPage_Pagecomponents_PageComponents_Cta1 {
-        
+}
+
+fragment Page_Cta1 on WpPage_Pagecomponents_PageComponents_Cta1 {
   body
   title
   subheader
@@ -174,213 +141,150 @@
       }
     }
   }
-    
+}
+
+fragment Page_Cta2 on WpPage_Pagecomponents_PageComponents_Cta2 {
+  title
+  body
+  image {
+    altText
+    localFile {
+      childImageSharp {
+        gatsbyImageData
       }
-     
- 
-      fragment Page_Cta2 on WpPage_Pagecomponents_PageComponents_Cta2 {
-        
-      title
-      body
-      image {
-          altText
-          localFile {
-              childImageSharp {
-                  gatsbyImageData
-              }
-          }
-      }
-      
-      }
-     
- 
-      fragment Page_Ebook on WpPage_Pagecomponents_PageComponents_Ebook {
-        
-    title
-    image {
-        altText
-        localFile {
-            childImageSharp {
-                gatsbyImageData
-            }
-        }
     }
-    
+  }
+}
+
+fragment Page_Ebook on WpPage_Pagecomponents_PageComponents_Ebook {
+  title
+  image {
+    altText
+    localFile {
+      childImageSharp {
+        gatsbyImageData
       }
-     
- 
-      fragment Page_Faq on WpPage_Pagecomponents_PageComponents_Faq {
-        
+    }
+  }
+}
+
+fragment Page_Faq on WpPage_Pagecomponents_PageComponents_Faq {
   title
   body
   questions {
     question
     answer
   }
-  
+}
+
+fragment Page_HeroSlider on WpPage_Pagecomponents_PageComponents_HeroSlider {
+  title
+}
+
+fragment Page_ImageGallery on WpPage_Pagecomponents_PageComponents_ImageGallery {
+  body
+  title
+  subheader
+  imageGallery {
+    altText
+    localFile {
+      childImageSharp {
+        gatsbyImageData
       }
-     
- 
-      fragment Page_HeroSlider on WpPage_Pagecomponents_PageComponents_HeroSlider {
-        
-    title
-      
+    }
+  }
+}
+
+fragment Page_ImageLeft on WpPage_Pagecomponents_PageComponents_ImageLeft {
+  subheader
+  title
+  body
+  image {
+    altText
+    localFile {
+      childImageSharp {
+        gatsbyImageData
       }
-     
- 
-      fragment Page_ImageGallery on WpPage_Pagecomponents_PageComponents_ImageGallery {
-        
-            body
-            title
-            subheader
-            imageGallery {
-              altText
-              localFile {
-                childImageSharp {
-                  gatsbyImageData
-                }
-              }
-            }
-    
+    }
+  }
+}
+
+fragment Page_ImageLeftDark on WpPage_Pagecomponents_PageComponents_ImageLeftDark {
+  subheader
+  title
+  body
+  image {
+    altText
+    localFile {
+      childImageSharp {
+        gatsbyImageData
       }
-     
- 
-      fragment Page_ImageLeft on WpPage_Pagecomponents_PageComponents_ImageLeft {
-        
-          subheader
-          title
-          body
-          image {
-              altText
-              localFile {
-                childImageSharp {
-                  gatsbyImageData
-                }
-              }
-            }
-      
+    }
+  }
+}
+
+fragment Page_ImageRight on WpPage_Pagecomponents_PageComponents_ImageRight {
+  subheader
+  title
+  body
+  image {
+    altText
+    localFile {
+      childImageSharp {
+        gatsbyImageData
       }
-     
- 
-      fragment Page_ImageLeftDark on WpPage_Pagecomponents_PageComponents_ImageLeftDark {
-        
-            subheader
-            title
-            body
-            image {
-                altText
-                localFile {
-                  childImageSharp {
-                    gatsbyImageData
-                  }
-                }
-              }
-        
-      }
-     
- 
-      fragment Page_ImageRight on WpPage_Pagecomponents_PageComponents_ImageRight {
-        
-        subheader
-        title
-        body
-        image {
-            altText
-            localFile {
-              childImageSharp {
-                gatsbyImageData
-              }
-            }
-          }
-    
-      }
-     
- 
-      fragment Page_ImageSlider on WpPage_Pagecomponents_PageComponents_ImageSlider {
-        
-    subheader
-    title
-    body
-  
-      }
-     
- 
-      fragment Page_ProcessA on WpPage_Pagecomponents_PageComponents_ProcessA {
-        
+    }
+  }
+}
+
+fragment Page_ImageSlider on WpPage_Pagecomponents_PageComponents_ImageSlider {
+  subheader
+  title
+  body
+}
+
+fragment Page_ProcessA on WpPage_Pagecomponents_PageComponents_ProcessA {
   title
   description
   processContent {
     number
     processDescription
   }
-  
-      }
-     
- 
-      fragment Page_ProcessB on WpPage_Pagecomponents_PageComponents_ProcessB {
-        
+}
+
+fragment Page_ProcessB on WpPage_Pagecomponents_PageComponents_ProcessB {
+  title
+  subheader
+  body
+  processContent {
+    number
     title
-    subheader
     body
-    processContent {
-      number
-      title
-      body
-    }
-    
-      }
-     
- 
-      fragment Page_RelatedServices on WpPage_Pagecomponents_PageComponents_RelatedServices {
-        
-    serviceContent {
-      title
-      link
-      image {
-        altText
-        localFile {
-          childImageSharp {
-            gatsbyImageData
-          }
+  }
+}
+
+fragment Page_RelatedServices on WpPage_Pagecomponents_PageComponents_RelatedServices {
+  serviceContent {
+    title
+    link
+    image {
+      altText
+      localFile {
+        childImageSharp {
+          gatsbyImageData
         }
       }
     }
-      
-      }
-     
- 
-      fragment Page_ServiceArea on WpPage_Pagecomponents_PageComponents_ServiceArea {
-        
-    title
-    content1
-    content2
-    
-      }
-     
- 
-      fragment Page_TabsSide on WpPage_Pagecomponents_PageComponents_TabsSide {
-        
-    title
-    tabsContent {
-      tabTitle
-      tabList
-      tabLabel
-      tabBody
-      tabImage {
-        altText
-        localFile {
-          childImageSharp {
-            gatsbyImageData
-          }
-        }
-      }
-    }
-    
-      }
-     
- 
-      fragment Page_TabsTop on WpPage_Pagecomponents_PageComponents_TabsTop {
-        
+  }
+}
+
+fragment Page_ServiceArea on WpPage_Pagecomponents_PageComponents_ServiceArea {
+  title
+  content1
+  content2
+}
+
+fragment Page_TabsSide on WpPage_Pagecomponents_PageComponents_TabsSide {
   title
   tabsContent {
     tabTitle
@@ -396,26 +300,36 @@
       }
     }
   }
-  
+}
+
+fragment Page_TabsTop on WpPage_Pagecomponents_PageComponents_TabsTop {
+  title
+  tabsContent {
+    tabTitle
+    tabList
+    tabLabel
+    tabBody
+    tabImage {
+      altText
+      localFile {
+        childImageSharp {
+          gatsbyImageData
+        }
       }
-     
- 
-      fragment Page_Testimonial on WpPage_Pagecomponents_PageComponents_Testimonial {
-        
-    title
-    testimonials {
-        testimonialBody
-        testimonialTitle
-        testimonialName
     }
-    
-      }
-     
- 
-      fragment Page_Text on WpPage_Pagecomponents_PageComponents_Text {
-        
-      text
-      }
-    
-    `
+  }
+}
+
+fragment Page_Testimonial on WpPage_Pagecomponents_PageComponents_Testimonial {
+  title
+  testimonials {
+    testimonialBody
+    testimonialTitle
+    testimonialName
+  }
+}
+
+fragment Page_Text on WpPage_Pagecomponents_PageComponents_Text {
+  text
+}`
     
